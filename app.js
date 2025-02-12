c847b7e10bb0693d85caee2215f1d8395fbb8c13
JOAO VICTOR DA SILVA GUILHERME <joao.vsguilherme@senacsp.edu.br>
2025-02-11 22:09:41 -0300

    app off-line

--- app.js
+++ app.js
@@ -4,16 +4,29 @@
  * @version 2.0
  */
 
+//===========================================
+//Registro do service worker
+
+// se o navegador de internet suportar este recurso
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .then(() => {
+            console.log("Service worker registrado!")
+        })
+}
+//===========================================
+
 function calcular() {
     //capturar os valores das caixas de input
-    let gasolina = document.getElementById('gasolina').value 
-    let etanol = document.getElementById('etanol').value 
+    let gasolina = document.getElementById('gasolina').value
+    let etanol = document.getElementById('etanol').value
     let kmlGasolina = document.getElementById('kmGasolina').value
     let kmlEtanol = document.getElementById('kmEtanol').value
-   //calculo da vantagem 
-   if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
-    document.getElementById('status').src = "img/etanol.png"
-   } else {
-    document.getElementById('status').src = "img/gasolina.png"
-   }
+    //cálculo da vantagem
+    if (etanol < (kmlEtanol / kmlGasolina) * gasolina) {
+        document.getElementById('status').src = "img/etanol.png"
+    } else {
+        document.getElementById('status').src = "img/gasolina.png"
+    }
 }
---
+++ sw.js
@@ -0,0 +1,40 @@
+/**
+ * Service Worker
+ * @author João Victor
+ */
+
+//instalação (cache "armazenamento local")
+self.addEventListener('install',(event) => {
+    event.waitUntill(
+        caches.open('static')
+        .then ((cache) => {
+            cache.add('/flexv2/')
+            cache.add('/flexv2/index.html')
+            cache.add('/flexv2/style.css')
+            cache.add('/flexv2/app.js')
+            cache.add('/flexv2/img/flex.png')
+            cache.add('/flexv2/img/calcflex.png')
+            cache.add('/flexv2/img/etanol.png')
+            cache.add('/flexv2/img/gasolina.png')
+        })
+    )
+})
+//ativação 
+self.addEventListener('activate',(event) => {
+    console.log("ativando o serviço worker...",event)
+    return self.clients.claim()
+})
+
+//interceptação (solicitação https servindo em cache quando off-line)
+self.addEventListener('fetch', (event) => {
+    event.respondiWicth(
+        caches.match(event.request)
+        .then((response) => {
+            if(response) {
+                return response
+            } else {
+                return fetch(event.request)
+            }
+        })
+    )
+})
